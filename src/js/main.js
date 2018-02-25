dffda5361c2737542a85bc16500ee88cd1527379
jamesonamadeus <jamesonamadeus@gmail.com>
2018-02-25 02:25:10 -0500

    New smoothscroll that syncs with gulp

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,9 @@
-//smooth scrolling 
-function anchorLinkHandler(e) {
-    const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-
-    // e.preventDefault();
-    // const targetID = this.getAttribute("href");
-    // const targetAnchor = document.querySelector(targetID);
-    // if (!targetAnchor) return;
-    // const originalTop = distanceToTop(targetAnchor);
-
-    // window.scrollBy({ top: originalTop - 100, left: 0, behavior: "smooth" });
-
-    // const checkIfDone = setInterval(function() {
-    //     const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-    //     if (distanceToTop(targetAnchor) === 0 || atBottom) {
-    //         targetAnchor.tabIndex = "-1";
-    //         targetAnchor.focus();
-    //         window.history.pushState("", "", targetID);
-    //         clearInterval(checkIfDone);
-    //     }
-    // }, 100);
-    
-}
-
-// const linksToAnchors = document.querySelectorAll('a[href^="#"]');
-
-// linksToAnchors.forEach(each => (each.onclick = anchorLinkHandler));
-
+//smooth scrolling
+var scroll = new SmoothScroll('a[href*="#"]',{
+    // Speed & Easing
+	speed: 500, // Integer. How fast to complete the scroll in milliseconds
+	offset: 100 // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+});
 
 //typing animation
 var options = {
