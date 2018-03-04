e90577a254ab5c1209ac9abb6d996bd90045f3b9
jamesonamadeus <jamesonamadeus@gmail.com>
2018-03-03 20:12:10 -0500

    Add in brgr script. Update the var for primary, secondary, & tertiary color. Update style on the brgr list.

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,18 @@ var scroll = new SmoothScroll('a[href*="#"]',{
 });
 
 //nav brgr
-var body = document.body;
-var burgerMenu = document.getElementsByClassName('nav-menu')[0];
-var burgerContain = document.getElementsByClassName('nav-contain')[0];
-var burgerNav = document.getElementsByClassName('nav-list')[0];
+(function() {
+    var body = document.body;
+    var burgerMenu = document.getElementsByClassName('nav-menu')[0];
+    var burgerContain = document.getElementsByClassName('nav-contain')[0];
+    var burgerNav = document.getElementsByClassName('nav-list')[0];
+  
+    burgerMenu.addEventListener('click', function toggleClasses() {
+      [body, burgerContain, burgerNav].forEach(function (el) {
+        el.classList.toggle('open');
+      });
+    }, false);
+})();
 
 //scroll reveal
 window.sr = ScrollReveal();
